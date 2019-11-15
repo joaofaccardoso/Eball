4f28c2db646c0333def1dfdedca115927dce8fe6
mcarolina5 <47608640+mcarolina5@users.noreply.github.com>
2019-11-15 00:56:19 +0000

    Update urls.py

--- appEball/urls.py
+++ appEball/urls.py
@@ -5,6 +5,9 @@ app_name = 'appEball'
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
+    path('navbar/', views.navbar, name='navbar'),
     path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
     path('teams_list/', views.teams_list, name='teams_list'),
+    path('user_profile/', views.user_profile, name='user_profile'),
 ]
