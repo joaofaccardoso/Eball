a161326d577ea57da84614f65ae2ec8c1cdc42d8
barbaravieira99 <barbara.vieira99@gmail.com>
2019-11-17 22:30:15 +0000

    edit profile

--- appEball/urls.py
+++ appEball/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import UserRegister, UserLogin, HomePage
+from .views import UserRegister, UserLogin, HomePage,edit_user_profile
 
 app_name = 'appEball'
 
@@ -11,5 +11,6 @@ urlpatterns = [
     path('logout/', views.userLogout, name='logout'),
     path('teams_list/', views.teams_list, name='teams_list'),
     path('profile/<str:username>', views.user_profile, name='userProfile'),
+    path('editprofile/<str:username>', edit_user_profile.as_view(), name='editUserProfile'),
     path('help/', views.help, name='help'),
 ]
