29db84c3aeaea20d8648f93aeb5de98929cee0e0
mcarolina5 <47608640+mcarolina5@users.noreply.github.com>
2019-11-19 20:46:01 +0000

    Update views.py

--- appEball/views.py
+++ appEball/views.py
@@ -1,10 +1,18 @@
+import json
 from django.shortcuts import render, reverse
 from django.contrib import messages
 from django.contrib.auth import logout, authenticate, login
 from django.http import HttpResponseRedirect
+from django.http import Http404
 from django.views import View
+from .forms import CustomUserForm, CustomUserLoginForm, EditProfileForm,TournamentCreationForm, TeamCreationForm
+from .models import CustomUser, Tournament, Team
 from .forms import CustomUserForm, CustomUserLoginForm, EditProfileForm
-from .models import CustomUser
+from .models import CustomUser,Notification
+from django.http import JsonResponse
+from django.db import transaction
+from django.db import IntegrityError
+
 
 class HomePage(View):
     template_name = 'appEball/home_page.html'
@@ -28,12 +36,18 @@ class UserRegister(View):
             user = authenticate(username=email, password=password)
             if(user.username == "admin"):
                 user.isAccepted = True
+                user.isTournamentManager = True
+            else:
+                admin = CustomUser.objects.filter(username="admin")
+                n = Notification(text = "New Register!\n"+user.username+" registered.", title = "New Register", user = admin[0])
+                n.save()
+            n = Notification(text = "Welcome to Eball!\nHere you can play in different teams with different people.\nJust waiting for admin confirmation.", title = "Welcome to Eball!", user = user)
+            n.save()
             login(request, user)
             messages.success(request, 'Account created successfuly!')
             return HttpResponseRedirect(reverse('appEball:home_page'))
         else:
-            print(form.errors)
-            messages.warning(request, 'Form is not valid.')
+            messages.warning(request, f'Form is not valid.')
             return HttpResponseRedirect(reverse('appEball:register'))
 
 class UserLogin(View):
@@ -64,7 +78,50 @@ def userLogout(request):
     return HttpResponseRedirect(reverse('appEball:home_page'))
 
 def teams_list(request):
-    return render(request, 'appEball/teams_list.html', {})
+    teams_initial=list(Team.objects.all())
+    teams=[]
+    for i in range(len(teams_initial)):
+        print("oi")
+        if(i%2==0):
+            teams.append(["row2",teams_initial[i]])
+        else:
+            teams.append(["row1",teams_initial[i]])
+
+    return render(request, 'appEball/teams_list.html', {'teams_list':teams})
+
+
+
+
+class new_team(View):
+    form_class = TeamCreationForm
+    template_name = 'appEball/new_team.html'
+
+    def get(self, request):
+        return render(request, self.template_name,{'tactics':TeamCreationForm.tacticChoice})
+
+    def post(self, request):
+        if request.method=="POST":
+            form = self.form_class(data=request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Team created successfuly!')
+                return HttpResponseRedirect(reverse('appEball:teams_list'))
+            else:
+                print(form.errors)
+                messages.warning(request, f'Form is not valid.')
+                return HttpResponseRedirect(reverse('appEball:new_team'))
+
+def tournaments(request):
+    tournaments_initial=list(Tournament.objects.all())
+    tournaments=[]
+    for i in range(len(tournaments_initial)):
+        if(i%2==0):
+            tournaments.append(["row2",tournaments_initial[i]])
+        else:
+            tournaments.append(["row1",tournaments_initial[i]])
+
+    return render(request, 'appEball/tournaments.html', {'tournaments':tournaments})
+
 
 def user_profile(request, username):
 	requestedUser = CustomUser.objects.get(username=username)
@@ -90,43 +147,51 @@ class edit_user_profile(View):
                 messages.success(request, ' Profile edited successfuly!')
                 return HttpResponseRedirect(reverse('appEball:userProfile',kwargs= {"username":username}))
             else:
-                print(form.errors )
+                print(form.errors)
                 messages.warning(request, f'Form is not valid.')
                 return HttpResponseRedirect(reverse('appEball:editUserProfile',kwargs= {"username":username}))
 
 def help(request):
 	return render(request,'appEball/help.html',{})
 
+
+
 def users(request):
+    allUsers = list()
     acceptedUsers = list()
     notAcceptedUsers = list()
     tournamentsManagers = list()
+    allFilter = list(CustomUser.objects.all().exclude(username="admin").order_by('isAccepted','username'))
     acceptedFilter = list(CustomUser.objects.filter(isAccepted = True).exclude(username="admin").order_by('username'))
     notAcceptedFilter = list(CustomUser.objects.filter(isAccepted = False).exclude(username="admin").order_by('username'))
     tournamentsManagersFilter = list(CustomUser.objects.filter(isTournamentManager = True).exclude(username="admin").order_by('username'))
+    for i in range(len(allFilter)):
+        if(i%2==0):
+            allUsers.append(["row2",allFilter[i]])
+            if(i<len(acceptedFilter)):
+                acceptedUsers.append(["row2",acceptedFilter[i]])
+            if(i<len(notAcceptedFilter)):
+                notAcceptedUsers.append(["row2",notAcceptedFilter[i]])
+            if(i<len(tournamentsManagersFilter)):
+                tournamentsManagers.append(["row2",tournamentsManagersFilter[i]])
+        else:
+            allUsers.append(["row1",allFilter[i]])
+            if(i<len(acceptedFilter)):
+                acceptedUsers.append(["row1",acceptedFilter[i]])
+            if(i<len(notAcceptedFilter)):
+                notAcceptedUsers.append(["row1",notAcceptedFilter[i]])
+            if(i<len(tournamentsManagersFilter)):
+                tournamentsManagers.append(["row1",tournamentsManagersFilter[i]])
 
-    for i in range(len(acceptedFilter)):
-        print(acceptedFilter[i].username)
-        if(i%2==0):
-            acceptedUsers.append(["row2",acceptedFilter[i]])
-        else:
-            acceptedUsers.append(["row1",acceptedFilter[i]])
-    for i in range(len(notAcceptedFilter)):
-        if(i%2==0):
-            notAcceptedUsers.append(["row2",notAcceptedFilter[i]])
-        else:
-            notAcceptedUsers.append(["row1",notAcceptedFilter[i]])
-    for i in range(len(tournamentsManagersFilter)):
-        if(i%2==0):
-            tournamentsManagers.append(["row2",tournamentsManagersFilter[i]])
-        else:
-            tournamentsManagers.append(["row1",tournamentsManagersFilter[i]])
-    return render(request,'appEball/users.html',{'acceptedUsers':acceptedUsers,'notAcceptedUsers':notAcceptedUsers,'tournamentsManagers':tournamentsManagers})
+    return render(request,'appEball/users.html',{'allUsers':allUsers,'acceptedUsers':acceptedUsers,'notAcceptedUsers':notAcceptedUsers,'tournamentsManagers':tournamentsManagers})
 
 def accept_user(request, username):
     requestedUser = CustomUser.objects.get(username=username)
     requestedUser.isAccepted = True
     requestedUser.save()
+    print("accept")
+    n = Notification(text = "You have been accepted to the app! Check the teams in need of an element.\n Hope you enjoy!", title = "You have been accepted to the app!", user = requestedUser)
+    n.save()
     return HttpResponseRedirect(reverse('appEball:users'))
 
 def delete_user(request, username):
@@ -135,13 +200,76 @@ def delete_user(request, username):
 
 def is_tournament_manager(request, username):
     requestedUser = CustomUser.objects.get(username=username)
-    if(requestedUser.isTournamentManager):
+    print("tuorn:",requestedUser.isTournamentManager)
+    if(requestedUser.isTournamentManager == True):
         requestedUser.isTournamentManager = False
+        n = Notification(text = "You are no more tournaments manager! You can still create and manage teams or play in another tournaments!", title = "You are no more tournaments manager!", user = requestedUser)
+        print("nhecas")
     else:
         requestedUser.isTournamentManager = True
+        n = Notification(text = "You are now tournaments manager! You can create and manage tournaments!", title = "You are now tournaments manager!", user = requestedUser)
+        print("Tornei gestor de torneios")
     requestedUser.save()
+    n.save()
     return HttpResponseRedirect(reverse('appEball:users'))
 
+
+
+class new_tournament(View):
+    form_class = TournamentCreationForm
+    template_name = 'appEball/new_tournament.html'
+
+    def get(self, request):
+        return render(request, self.template_name,{'week':TournamentCreationForm.week})
+
+    def post(self, request):
+        if request.method=="POST":
+            form = self.form_class(data=request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Tournament created successfuly!')
+                return HttpResponseRedirect(reverse('appEball:tournaments'))
+            else:
+                print(form.errors)
+                messages.warning(request, f'Form is not valid.')
+                return HttpResponseRedirect(reverse('appEball:new_tournament'))
+def notifications(request):
+    notifications = list()
+    notificationsNotSeen = list()
+    notificationsFilter = Notification.objects.filter(user = request.user).order_by('isSeen','-date')
+    for i in range(len(notificationsFilter)):
+        print("olha ue lindo:",notificationsFilter[i].isSeen)
+        if(i%2==0):
+            notifications.append(["row2","collapseMessage"+str(i),notificationsFilter[i]])
+        else:
+            notifications.append(["row1","collapseMessage"+str(i),notificationsFilter[i]])
+    notificationsNotSeenFilter = Notification.objects.filter(user = request.user, isSeen = False).order_by('isSeen','-date')
+    for i in range(len(notificationsNotSeenFilter)):
+        if(i%2==0):
+            notificationsNotSeen.append(["row2","collapseMessage"+str(i),notificationsNotSeenFilter[i]])
+        else:
+            notificationsNotSeen.append(["row1","collapseMessage"+str(i),notificationsNotSeenFilter[i]])
+    return render(request, 'appEball/notifications.html', {'notifications':notifications,'notificationsNotSeen':notificationsNotSeen})
+
+def is_seen(request, pk):
+    if request.user.is_authenticated:
+        request_data = json.loads(request.body)
+        try:
+            with transaction.atomic():
+                notification = Notification.objects.get(pk=pk)
+                notification.isSeen = request_data.get("isSeen")
+                notification.save()
+                notifications = list()
+                notificationsFilter = Notification.objects.filter(user = request.user).order_by('-date','-isSeen')
+                for i in range(len(notificationsFilter)):
+                    if(i%2==0):
+                        notifications.append(["row2","collapseMessage"+str(i),notificationsFilter[i]])
+                    else:
+                        notifications.append(["row1","collapseMessage"+str(i),notificationsFilter[i]])
+                return JsonResponse({"success": True})
+        except IntegrityError as err:
+            raise err
+    raise Http404
 
 
 def askSub(request):
