053bb7e4ff5d5c4e1e06b15d510ed492c55b0da2
mcarolina5 <47608640+mcarolina5@users.noreply.github.com>
2019-11-19 22:02:40 +0000

    Update urls.py

--- appEball/urls.py
+++ appEball/urls.py
@@ -5,11 +5,14 @@ from .views import UserRegister, UserLogin, HomePage,edit_user_profile, new_tour
 app_name = 'appEball'
 
 urlpatterns = [
-    path('', HomePage.as_view(), name='home_page'),
-    path('register/', UserRegister.as_view(), name='register'),
-    path('login/', UserLogin.as_view(), name='login'),
+    path('', views.HomePage.as_view(), name='home_page'),
+    path('register/', views.UserRegister.as_view(), name='register'),
+    path('login/', views.UserLogin.as_view(), name='login'),
     path('logout/', views.userLogout, name='logout'),
     path('teams_list/', views.teams_list, name='teams_list'),
+    path('new_team/', new_team.as_view(), name='new_team'),
+    path('tournaments/', views.tournaments, name='tournaments'),
+    path('tournaments/new/', new_tournament.as_view(), name='new_tournament'),
     path('profile/<str:username>', views.user_profile, name='userProfile'),
     path('editprofile/<str:username>', edit_user_profile.as_view(), name='editUserProfile'),
     path('help/', views.help, name='help'),
